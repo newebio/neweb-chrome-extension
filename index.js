103f183c9b4a7aec68f51f50b098751d759423bd
Vitaly Arkhipov <arvitaly@list.ru>
2018-06-28 01:15:52 +0300

    added exports

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./lib/ChromeClient"));
 __export(require("./lib/ChromeClientTransport"));
 __export(require("./lib/ChromeServerTransport"));
 __export(require("./lib/ChromeServerTransportClient"));
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export * from "./lib/ChromeClient";
 export * from "./lib/ChromeClientTransport";
 export * from "./lib/ChromeServerTransport";
 export * from "./lib/ChromeServerTransportClient";
