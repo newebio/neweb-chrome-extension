5ef93a13cfe644112d668449ecd03f4bb4273908
Vitaly Arkhipov <arvitaly@list.ru>
2018-06-28 01:23:29 +0300

    added exports

--- lib/ChromeServerTransport.ts
+++ lib/ChromeServerTransport.ts
@@ -1,7 +1,7 @@
 import { IServerTransport, IServerTransportClient } from "neweb-core";
 import { Subject } from "rxjs";
 import { ChromeServerTransportClient } from "./ChromeServerTransportClient";
-class ChromeServerTransport implements IServerTransport {
+export class ChromeServerTransport implements IServerTransport {
     onConnect = new Subject<IServerTransportClient>();
     constructor() {
         chrome.runtime.onConnect.addListener((port) => this.onConnect.next(new ChromeServerTransportClient({ port })));
